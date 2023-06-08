3b0eb2c79fc7ff1e090590bdc5ac879bba94ace0
LydRod206 <119384486+LydRod206@users.noreply.github.com>
2023-06-08 12:21:56 -0700

    some more routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,15 @@
 const router = require('express').Router();
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
+const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
+
+router.use('/user', userRoutes);
+router.use('/post', postRoutes);
+router.use('/comment', commentRoutes);
 router.use('/', homeRoutes);
-router.use('/', dashboardRoutes);
+router.use('/dashboard', dashboardRoutes);
 router.use((req, res) => {
     res.status(404).end();
 });
